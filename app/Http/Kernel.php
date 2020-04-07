fb00fe96ead3c7ab27bc1d8f23b597930ae3f817
AI1411 <goethe0719@gmail.com>
2020-04-07 11:28:25 +0900

    add cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Http;
 
 use App\Http\Middleware\ProfileJsonResponse;
+use Fruitcake\Cors\HandleCors;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -21,6 +22,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         ProfileJsonResponse::class,
+        HandleCors::class,
     ];
 
     /**
